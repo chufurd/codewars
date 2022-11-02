4595ccb1dbc9ad1c90ab485dfc57c3c89bcd527d
Jesus Rodriguez <jrod1841@gmail.com>
2022-11-01 19:39:39 -0500

    codewars for today

--- Freudiantranslator.js
+++ Freudiantranslator.js
@@ -12,3 +12,19 @@ function toFreud(string) {
   return string.replace(/[^ ]+/g,'sex')
 
 }
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number=function(array){
+  const line = array.map((n, i) => `${i + 1}: ${n}`)
+  return line
+}
