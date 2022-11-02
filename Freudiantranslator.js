d43d32b1cb355a6e21fd8ce6bd1abe5ccf60b882
Jesus Rodriguez <jrod1841@gmail.com>
2022-11-01 20:12:57 -0500

    codewars for today

--- Freudiantranslator.js
+++ Freudiantranslator.js
@@ -28,3 +28,18 @@ var number=function(array){
   const line = array.map((n, i) => `${i + 1}: ${n}`)
   return line
 }
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+// i.e.
+
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+function friend(friends){
+  const sort = friends.filter(num => num.length === 4 ? num : false )
+  return sort
+}
