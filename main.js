af15d4f0e605e71bee7da237ccaf3314cdee5b82
Jesus Rodriguez <jrod1841@gmail.com>
2022-11-05 16:17:15 -0500

    codewars

--- main.js
+++ main.js
@@ -9,3 +9,18 @@
 //practice 
 // test
 >>>>>>> a0cc0cc288eb75f1b2b5a1d641375e3d8a227c97
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  
+}
+
+function getDivisorsCnt(n){
+  var empty = 0
+ for (var i = 1; i <= n; i++) {
+  if(n % i === 0){
+   empty++
+  }
+   }
+return empty
+}
