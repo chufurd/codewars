c5108cef4151ed676e3915255714d4a81b9d90c3
Jesus Rodriguez <jrod1841@gmail.com>
2022-11-12 20:03:31 -0600

    codewars

--- main.js
+++ main.js
@@ -60,3 +60,18 @@ function minMax(arr){
 function sortByLength (array) {
   return array.sort((a, b) => a.length > b.length ? 1 : -1)
 };
+
+function getGrade (s1, s2, s3) {
+  const total = (s1 + s2 + s3) / 3
+  if(total >= 90 ){
+    return 'A'
+  }else if(total >= 80){
+    return 'B'
+  }else if(total >= 70){
+    return 'C'
+  }else if(total >= 60){
+    return 'D'
+  }else{
+    return 'F'
+  }
+}
