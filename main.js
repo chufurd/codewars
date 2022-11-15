1462e99ddd6552e7607010aa42989f3e019c3190
Jesus Rodriguez <jrod1841@gmail.com>
2022-11-15 14:57:52 -0600

    codewars practice

--- main.js
+++ main.js
@@ -50,28 +50,9 @@ function minMax(arr){
   const last = arr.sort((a, b) => a > b ? 1 : -1).slice(-1)[0]
   const first = arr.sort((a, b) => a > b ? -1 : 1).slice(-1)[0]
   return [first, last]
-<<<<<<< HEAD
-} 
-=======
 >>>>>>> 5b75b6604cab577df012dc1ee5ab0848b5fce2a8
 }
->>>>>>> 0e031d644f57321b774f8f4a061201d581ef001c
-  
-function sortByLength (array) {
-  return array.sort((a, b) => a.length > b.length ? 1 : -1)
-};
 
-function getGrade (s1, s2, s3) {
-  const total = (s1 + s2 + s3) / 3
-  if(total >= 90 ){
-    return 'A'
-  }else if(total >= 80){
-    return 'B'
-  }else if(total >= 70){
-    return 'C'
-  }else if(total >= 60){
-    return 'D'
-  }else{
-    return 'F'
-  }
+function findDifference(a, b) {
+  return Math.abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]))
 }
