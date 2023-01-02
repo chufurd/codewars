6170e2aff6a44c3e2bbdce58dcc28cf61ec7ee1c
Jesus Rodriguez <jrod1841@gmail.com>
2023-01-02 13:13:29 -0600

    2023 starting right!

--- main.js
+++ main.js
@@ -80,3 +80,16 @@ else if (operation === '/') return value1 / value2
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals
 }
+
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+
+function smallEnough(a, limit){
+  for (let i = 0; i < a.length; i++){
+      if (a[i] > limit){
+        return false
+      }
+      }
+    return true
+  }
