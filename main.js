7e1c2cc7a37ff29b60ac369b06a8b8d18f93d017
Jesus Rodriguez <jrod1841@gmail.com>
2023-01-09 14:28:14 -0600

    codewars

--- main.js
+++ main.js
@@ -165,3 +165,11 @@ function removeExclamationMarks(s) {
   return check
   
 }
+
+function combat(health, damage) {
+  if((health - damage) > 0 ){
+    return health - damage
+  }else{
+    return 0
+  }
+ } 
