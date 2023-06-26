e246c4f14fc4288f1dd8100fd1e203a8fdceac14
Jesus Rodriguez <jrod1841@gmail.com>
2023-06-26 17:58:54 -0500

    codewars

--- main.js
+++ main.js
@@ -207,4 +207,26 @@ var capitals = function (word) {
 };
  
  
- 
+ function greet(language) {
+	const lang = {
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+
+  }
+  return lang[language] || 'Welcome'
+}
